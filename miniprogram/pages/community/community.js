2d7ec6b77ed8357285b7cd8d7b011480e017bf5d
lmmryt <893942648@qq.com>
2020-05-14 17:20:12 +0800

    5

--- miniprogram/pages/community/community.js
+++ miniprogram/pages/community/community.js
@@ -54,10 +54,31 @@ Page({
   delate() {
 
   },
+  // 删除二级评论
+  delcomment() {
+    wx.showModal({
+      title: '',
+      content: '确定要删除该评论？',
+      showCancel: true,//是否显示取消按钮
+      cancelColor: 'green',//取消文字的颜色
+      confirmText: "删除",//默认是“确定”
+      confirmColor: 'red',//确定文字的颜色
+      success: function (res) {
+        if (res.cancel) {
+          //点击取消,默认隐藏弹框
+        } else {
+          //点击确定
+          
+        }
+      },
+      fail: function (res) { },//接口调用失败的回调函数
+      complete: function (res) { },//接口调用结束的回调函数（调用成功、失败都会执行）
+    })
+  },
   // 回复评论
   comment(e) {
     console.log(e.currentTarget)
-    const {contentId,con} = e.currentTarget.dataset;
+    const { contentId, con } = e.currentTarget.dataset;
     wx.navigateTo({
       url: `/pages/community/publish/publish?commentId=${contentId}&show_publish=${con}`,
     })
